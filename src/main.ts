8e9155e8b8ac8d2dcbe2e0c03392275bc3ba7a3b
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-29 16:34:16 +0800

    feat: add exception error

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import { MyLogger } from './utils/logger'
 import { AppModule } from './app.module'
 import config from './config'
+import { AllExceptionsFilter } from './filters/all-exceptions.filter'
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, {
     logger: new MyLogger(),
   })
+  app.useGlobalFilters(new AllExceptionsFilter())
   app.enableCors()
 
   await app.listen(config.server.port)
