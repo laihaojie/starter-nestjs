c85fd55704dddab0e16ecfc1aef2cd1af16c3979
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-29 16:32:31 +0800

    chore: update

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,4 +1,3 @@
-const Response = (data: unknown, message = '成功') => ({ code: 1, data, message })
-Response.Error = (data: unknown, message = '服务器繁忙') => ({ code: 3, data, message })
-Response.None = (data: unknown, message = '用户未授权') => ({ code: 401, data, message })
-export default Response
+export const R = (data: unknown = {}, message = '成功') => ({ code: 1, data, message })
+export const E = (message = '服务器繁忙', data = {}) => ({ code: 3, data, message })
+export const N = (data: unknown, message = '用户未授权') => ({ code: 401, data, message })
