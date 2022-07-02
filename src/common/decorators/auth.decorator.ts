54a947527521a0211e3aa5efa0a52a28fe965e31
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-07-02 08:33:05 +0800

    feat: add user decorator

--- src/common/decorators/auth.decorator.ts
+++
@@ -1,3 +0,0 @@
-import { SetMetadata } from '@nestjs/common'
-
-export const Auth = (...args: string[]) => SetMetadata('auth', args)
--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,11 +1,16 @@
-import { createParamDecorator } from '@nestjs/common'
+import { ExecutionContext, SetMetadata, createParamDecorator } from '@nestjs/common'
 
 import * as requestIp from 'request-ip'
-
-export * from './auth.decorator'
 
 export const IpAddress = createParamDecorator((data, req) => {
   if (req.clientIp)
     return req.clientIp
   return requestIp.getClientIp(req) // In case we forgot to include requestIp.mw() in main.ts
+})
+
+export const Auth = (...args: string[]) => SetMetadata('auth', args)
+
+export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest()
+  return request.user
 })
