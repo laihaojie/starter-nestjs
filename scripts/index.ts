4de961907fb495188587aa2874bfa3b03455bf71
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-07-08 14:30:33 +0800

    feat: add genearte module

---
+++ scripts/index.ts
@@ -0,0 +1,55 @@
+import { execSync } from 'child_process'
+import { join } from 'path'
+import fs from 'fs'
+
+const path_name = process.argv[2]
+const source_path = join(process.cwd(), 'src/modules', path_name)
+
+const name = source_path.split('\\').reverse()[0]
+const obj = {
+  module: {
+    exist: false,
+    command: `nest g mo modules/${path_name}`,
+  },
+  controller: {
+    exist: false,
+    command: `nest g co modules/${path_name}`,
+  },
+  service: {
+    exist: false,
+    command: `nest g s modules/${path_name}`,
+  },
+}
+
+// 判断目录是否存在
+if (!fs.existsSync(source_path)) {
+  generate()
+}
+else {
+  deleteFile()
+  const cur = fs.readdirSync(source_path).map(file => [file.split('.')[1], file.split('.')[0]]).filter(i => i[1] === name)
+
+  const { controller, module, service } = Object.fromEntries(cur)
+  if (controller && controller === name) obj.controller.exist = true
+  if (module && module === name) obj.module.exist = true
+  if (service && service === name) obj.service.exist = true
+  generate()
+}
+
+function generate() {
+  const commands = Object.keys(obj).reduce((pre, key) => !obj[key].exist ? [...pre, obj[key].command] : pre, [])
+
+  commands.forEach((command) => {
+    execSync(command, { stdio: 'inherit' })
+  })
+
+  deleteFile()
+}
+
+function deleteFile() {
+  const files = fs.readdirSync(source_path).filter(file => file.endsWith('spec.ts'))
+  // 删除文件
+  files.forEach((file) => {
+    fs.unlinkSync(join(source_path, file))
+  })
+}
